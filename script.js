6ee36647d265c27ee13c27842b7e2cae063d907c
mallosrhis <166078730+mallosrhis@users.noreply.github.com>
2024-05-14 01:11:39 -0600

    Update script.js

--- script.js
+++ script.js
@@ -73,4 +73,18 @@ document.addEventListener('DOMContentLoaded', function() {
         });
       });
     });
+
+  // Sticky navbar
+  window.onscroll = function() {stickyNavbar()};
+
+  var navbar = document.getElementById("navbar");
+  var sticky = navbar.offsetTop;
+
+  function stickyNavbar() {
+    if (window.pageYOffset >= sticky) {
+      navbar.classList.add("sticky");
+    } else {
+      navbar.classList.remove("sticky");
+    }
+  }
 });
